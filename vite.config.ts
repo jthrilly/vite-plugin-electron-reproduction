982d750c5a4235eb5514eb3ccac3a290330f4753
Joshua Melville <joshua.melville@northwestern.edu>
2023-02-17 11:00:08 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron'
+
+export default defineConfig({
+  plugins: [
+    electron([
+      {
+        // Main-Process entry file of the Electron App.
+        entry: 'electron/main.ts',
+      },
+      {
+        entry: 'electron/preload.ts',
+        onstart(options: any) {
+          // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete, 
+          // instead of restarting the entire Electron App.
+          options.reload()
+        },
+      },
+    ]),
+  ],
+})
